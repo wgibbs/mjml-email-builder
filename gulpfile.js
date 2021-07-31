69285ca9f2d1e13312e8bacc07e8ff98d03455ca
Wesley Gibbs <wesleygibbs@Wesleys-Mac-mini.local>
2021-07-31 08:31:20 -0400

    More gulp updates

--- gulpfile.js
+++ gulpfile.js
@@ -5,17 +5,18 @@ mjmlEngine = require('mjml'),
 browserSync = require('browser-sync');
 
 const mjmlSrcPath = ['./source/index.mjml'];
+const buildDest = ['./build/'];
 
 function buildMjml() {
   gulp.src(mjmlSrcPath)
   .pipe(mjml(mjmlEngine, {minify: true}))
-  .pipe(gulp.dest('./build/'))
+  .pipe(gulp.dest(buildDest))
   .pipe(browserSync.stream());
 }
 
 function serve() {
   browserSync.init({
-    server: "./build/",
+    server: buildDest,
     port: 8000,
     open: true,
     notify: false
